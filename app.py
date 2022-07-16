04c30b609f030702d79ec6da451ae7ea2c75f2bb
Eric Zhu <ericxjzhu@gmail.com>
2022-07-17 00:52:47 +1000

    More basic frontend

--- app.py
+++ app.py
@@ -1,22 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    return render_template('index.html')
 
+@app.route("/", methods=['POST'])
+def search_url():
+    data = json.loads(request.data)
+    print(data["url"])
+    
+
+    # use the url past from the frontend, get the result, and pass back to frontend
     items = [
-        {'id': 1, 'title': "article1", 'url': "xxxx.com", 'reliablity': "80%", 'political leaning': "xxxxx"},
-        {'id': 2, 'title': "article2", 'url': "xxxx.com", 'reliablity': "80%", 'political leaning': "xxxxx"},
-        {'id': 3, 'title': "article3", 'url': "xxxx.com", 'reliablity': "80%", 'political leaning': "xxxxx"},
-        {'id': 4, 'title': "article4", 'url': "xxxx.com", 'reliablity': "80%", 'political leaning': "xxxxx"}
+        {'id': 1, 'title': "article1", 'url': "xxxx.com", 'reliability': "left", 'politicalLeaning': "xxxxx"},
+        {'id': 2, 'title': "article2", 'url': "xxxx.com", 'reliability': "right", 'politicalLeaning': "xxxxx"},
+        {'id': 3, 'title': "article3", 'url': "xxxx.com", 'reliability': "80", 'politicalLeaning': "xxxxx"},
+        {'id': 4, 'title': "article4", 'url': "xxxx.com", 'reliability': "80%", 'politicalLeaning': "xxxxx"}
     ]
+    
+    return json.dumps({'data': items})
 
-    return render_template('index.html', items=items)
 
-@app.route("/about/<username>")
-def about_page(username):
-    return f'<h1>About page with the user name: {username}</h1>'
 
 if __name__ == "__main__":
     app.run(debug = True)
