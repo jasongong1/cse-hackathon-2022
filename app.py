3f23d461f433a7b2651d8df209cd632f05494205
JayC-github <jianjunjchen@gmail.com>
2022-07-16 21:11:30 +1000

    basic frontend

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+
+    items = [
+        {'id': 1, 'title': "article1", 'url': "xxxx.com", 'reliablity': "80%", 'political leaning': "xxxxx"},
+        {'id': 2, 'title': "article2", 'url': "xxxx.com", 'reliablity': "80%", 'political leaning': "xxxxx"},
+        {'id': 3, 'title': "article3", 'url': "xxxx.com", 'reliablity': "80%", 'political leaning': "xxxxx"},
+        {'id': 4, 'title': "article4", 'url': "xxxx.com", 'reliablity': "80%", 'political leaning': "xxxxx"}
+    ]
+
+    return render_template('index.html', items=items)
+
+@app.route("/about/<username>")
+def about_page(username):
+    return f'<h1>About page with the user name: {username}</h1>'
+
+if __name__ == "__main__":
+    app.run(debug = True)
